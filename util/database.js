b2e2fe82923ae4e2f189789541ce4a6f6c381032
melissa wu <melissawu328@gmail.com>
2024-03-26 20:53:24 +1300

    connect to database

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '2024Bonjour,.',
-  database: 'node-complete'
+const sequelize = new Sequelize('node-complete', 'root', '2024Bonjour,.', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
